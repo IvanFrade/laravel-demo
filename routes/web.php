b2aa39bedab863603ae21b183878b9f88d6b983c
Ivan <fradellin.ivan@gmail.com>
2025-06-12 22:21:50 +0200

    Add post creation

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('index');
 });
 
+// User routes
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class, 'login']);
 Route::post('/logout', [UserController::class, 'logout']);
+
+// Post routes
+Route::post('/create-post', [PostController::class, 'createPost']);
