440bafb18e4658c4b933991bf557fe998d4a93d4
Ivan <fradellin.ivan@gmail.com>
2025-06-30 00:00:40 +0200

    Style admin lists

--- resources/views/dashboard_list.blade.php
+++ resources/views/dashboard_list.blade.php
@@ -1,124 +1,147 @@
-<x-layout>
-    
+<x-layout>    
     <x-admin-header />
 
     @if($table === 'books')
-        <h2>Libri censiti</h2>
+        <h2 class="text-center text-indigo-600 font-bold text-3xl py-4">Libri censiti</h2>
 
-        {{-- Check wether any loans are ongoing for current user --}}
         @if(!$data)
-        <h4>Nessun libro nel sistema!</h4>
+        <h4 class="text-center text-indigo-600 font-bold text-2xl py-4">Nessun libro nel sistema!</h4>
 
         @else
-        <table>
-            <tr>
-                <th>ISBN</th>
-                <th>Titolo</th>
-                <th>Autore</th>
-                <th>Editore</th>
-                <th>Anno</th>
-                <th>Descrizione</th>
-            </tr>
-            @foreach($data as $book)
-            <tr>
-                <td>{{ $book->isbn }}</td>              
-                <td>{{ $book->title }}</td>       
-                <td>{{ $book->author }}</td>
-                <td>{{ $book->publisher }}</td>
-                <td>{{ $book->year }}</td>
-                <td>{{ $book->description }}</td>
-            </tr>
-            @endforeach
-        </table>
+        <div class=" wrap-anywhere overflow-x-auto overflow-y-auto rounded border border-gray-300 shadow-sm m-5 md:max-h-[80vh]">
+            <table class="min-w-full divide-y-2 divide-gray-200 table-fixed text-center">
+                <thead class="ltr:text-left rtl:text-right">
+                    <tr class="*:font-medium *:text-gray-900 text-center">
+                        <th class="px-3 py-2 whitespace-nowrap w-[20%]">ISBN</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[20%]">Titolo</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[10%]">Autore</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[10%]">Editore</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[10%]">Anno</th>
+                        <th class="px-3 py-2 whitespace-normal break-words w-[30%]">Descrizione</th>
+                    </tr>
+                </thead>
+ 
+                <tbody class="divide-y divide-gray-200">
+                    @foreach($data as $book)
+                    <tr class="*:text-gray-900 *:first:font-medium">
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $book->isbn }}</td>              
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $book->title }}</td>       
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $book->author }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $book->publisher }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $book->year }}</td>
+                        <td class="px-3 py-2 whitespace-normal break-words">{{ $book->description }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         @endif
 
     @elseif($table === 'copies')
-        <h2>Copie</h2>
+        <h2 class="text-center text-indigo-600 font-bold text-3xl py-4">Copie </h2>
 
-        {{-- Check wether any loans are ongoing for current user --}}
         @if(!$data)
-        <h4>Nessuna copia nel sistema!</h4>
+        <h4 class="text-center text-indigo-600 font-bold text-2xl py-4">Nessuna copia nel sistema!</h4>
 
         @else
-        <table>
-            <tr>
-                <th>Barcode</th>
-                <th>ISBN</th>
-                <th>Titolo</th>
-                <th>Autore</th>
-                <th>Editore</th>
-                <th>Anno</th>
-                <th>Condizione</th>
-                <th>Disponibile</th>
-            </tr>
-            @foreach($data as $copy)
-            <tr>
-                <td>{{ $copy->copyId }}</td>  
-                <td>{{ $copy->isbn }}</td>              
-                <td>{{ $copy->title }}</td>       
-                <td>{{ $copy->author }}</td>
-                <td>{{ $copy->publisher }}</td>
-                <td>{{ $copy->year }}</td>
-                <td>{{ $copy->condition }}</td>
-                <td>{{ $copy->available ? 'Disponibile' : 'Prenotata' }}</td>
-            </tr>
-            @endforeach
-        </table>
+        <div class="overflow-x-auto overflow-y-auto rounded border border-gray-300 shadow-sm m-5 md:max-h-[80vh]">
+            <table class="min-w-full divide-y-2 divide-gray-200 table-fixed text-center">
+                <thead class="ltr:text-left rtl:text-right">
+                    <tr class="*:font-medium *:text-gray-900 text-center">
+                        <th class="px-3 py-2 whitespace-nowrap w-[7%]">Barcode</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[7%]">ISBN</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[20%]">Titolo</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[20%]">Autore</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[15%]">Editore</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[5%]">Anno</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[8%]">Condizione</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[8%]">Disponibile</th>
+                    </tr>
+                </thead>
+                
+                <tbody class="divide-y divide-gray-200">
+                    @foreach($data as $copy)
+                    <tr class="*:text-gray-900 *:first:font-medium">
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $copy->copyId }}</td>  
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $copy->isbn }}</td>              
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $copy->title }}</td>       
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $copy->author }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $copy->publisher }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $copy->year }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $copy->condition }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $copy->available ? 'Disponibile' : 'Prenotata' }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         @endif
 
     @elseif($table === 'loans')
-        <h2>Prestiti in corso</h2>
+        <h2 class="text-center text-indigo-600 font-bold text-3xl py-4">Prenotazioni in corso </h2>
 
-        {{-- Check wether any loans are ongoing for current user --}}
         @if(!$data)
-        <h4>Nessun prestito in corso!</h4>
+        <h4 class="text-center text-indigo-600 font-bold text-2xl py-4">Nessuna prenotazione in corso!</h4>
 
         @else
-        <table>
-            <tr>
-                <th>Barcode</th>
-                <th>Utente</th>
-                <th>Titolo</th>
-                <th>Autore</th>
-                <th>Anno</th>
-                <th>Condizione</th>
-                <th>Inizio prestito</th>
-            </tr>
-            @foreach($data as $copy)
-            <tr>
-                <td>{{ $copy->copyId    }}</td>              
-                <td>{{ $copy->username }}</td>       
-                <td>{{ $copy->title }}</td>
-                <td>{{ $copy->author }}</td>
-                <td>{{ $copy->year }}</td>
-                <td>{{ $copy->condition }}</td>
-                <td>{{ \Carbon\Carbon::parse($copy->borrowed_at)->locale('it')->translatedFormat('d F Y') }}</td>
-            </tr>
-            @endforeach
-        </table>
+        <div class="overflow-x-auto overflow-y-auto rounded border border-gray-300 shadow-sm m-5 md:max-h-[80vh]">
+            <table class="min-w-full divide-y-2 divide-gray-200 table-fixed text-center">
+                <thead class="ltr:text-left rtl:text-right">
+                    <tr class="*:font-medium *:text-gray-900 text-center">
+                        <th class="px-3 py-2 whitespace-nowrap w-[10%]">Barcode</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[10%]">Utente</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[20%]">Titolo</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[20%]">Autore</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[1%]">Anno</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[10%]">Condizione</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[20%]">Inizio prestito</th>
+                    </tr>
+                </thead>
+            
+                <tbody class="divide-y divide-gray-200">
+                    @foreach($data as $loan)
+                    <tr class="*:text-gray-900 *:first:font-medium">
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $loan->copyId }}</td>              
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $loan->username }}</td>       
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $loan->title }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $loan->author }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $loan->year }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $loan->condition }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ \Carbon\Carbon::parse($loan->borrowed_at)->locale('it')->translatedFormat('d F Y') }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
         @endif
 
     @elseif($table === 'users')
-        <h2>Utenti registrati</h2>
+        <h2 class="text-center text-indigo-600 font-bold text-3xl py-4">Utenti registrati </h2>
 
-        <table>
-            <tr>
-                <th>ID</th>
-                <th>Username</th>
-                <th>Email</th>
-                <th>Admin</th>
-                <th>Data creazione</th>
-            </tr>
-            @foreach($data as $user)
-            <tr>
-                <td>{{ $user->id }}</td>              
-                <td>{{ $user->username }}</td>       
-                <td>{{ $user->email }}</td>
-                <td>{{ $user->isAdmin ? "Si" : "No" }}</td>
-                <td>{{ \Carbon\Carbon::parse($user->created_at)->locale('it')->translatedFormat('d F Y') }}</td>
-            </tr>
-            @endforeach
-        </table>
+        <div class="overflow-x-auto overflow-y-auto rounded border border-gray-300 shadow-sm m-5 md:max-h-[80vh]">
+            <table class="min-w-full divide-y-2 divide-gray-200 table-fixed text-center">
+                <thead class="ltr:text-left rtl:text-right">
+                    <tr class="*:font-medium *:text-gray-900 text-center">
+                        <th class="px-3 py-2 whitespace-nowrap w-[10%]">ID</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[25%]">Username</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[25%]">Email</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[15%]">Admin</th>
+                        <th class="px-3 py-2 whitespace-nowrap w-[25%]">Data creazione</th>
+                    </tr>
+                </thead>
+                
+                <tbody class="divide-y divide-gray-200">
+                    @foreach($data as $user)
+                    <tr class="*:text-gray-900 *:first:font-medium">
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $user->id }}</td>              
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $user->username }}</td>       
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $user->email }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ $user->isAdmin ? "Si" : "No" }}</td>
+                        <td class="px-3 py-2 whitespace-nowrap">{{ \Carbon\Carbon::parse($user->created_at)->locale('it')->translatedFormat('d F Y') }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     @endif
 
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -28,7 +28,7 @@
                 </label>
 
                 <button 
-                class="inline-block rounded-sm border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:ring-3 focus:outline-hidden mt-2">
+                class="inline-block rounded-sm border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 hover:cursor-pointer focus:ring-3 focus:outline-hidden mt-2">
                 Registrati
                 </button>
             </form>
@@ -50,7 +50,7 @@
                 </label>
                 
                 <button 
-                class="inline-block rounded-sm border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:ring-3 focus:outline-hidden mt-2">
+                class="inline-block rounded-sm border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 hover:cursor-pointer focus:ring-3 focus:outline-hidden mt-2">
                 Login
                 </button>
             </form>
